ffc5d04c59afc65ce04121a187fbb33eb809adb9
Rares Matei <matei.rar@gmail.com>
2021-09-06 23:36:40 +0300

    bike store

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   projects: [
-    '<rootDir>/apps/bike-store',
+    ,
     '<rootDir>/libs/common/ui-progress-meter',
     '<rootDir>/apps/order-pizza',
     '<rootDir>/libs/common/ui-image-radio-buttons',

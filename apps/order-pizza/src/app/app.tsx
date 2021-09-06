92b38c1cd87a321b8dc8b9fa895734c3fb6e901e
Rares Matei <matei.rar@gmail.com>
2021-09-06 23:22:38 +0300

    extract form

--- apps/order-pizza/src/app/app.tsx
+++ apps/order-pizza/src/app/app.tsx
@@ -20,6 +20,7 @@ import {
   CommonUiMultiSelect,
   CommonUiMultiSelectProps,
 } from '@storybook-test/common/ui-multi-select';
+import { CommonUiForm } from '@storybook-test/common/ui-form';
 
 export function App() {
   const elementRef = useRef();
@@ -69,7 +70,7 @@ export function App() {
 
   return (
     <div className={styles.app}>
-      <UiForm ref={elementRef} onChange={onChange}>
+      <CommonUiForm ref={elementRef} onChange={onChange}>
         <Heading as="h1" size="xl" textAlign="center">
           Nx Pizza
         </Heading>
@@ -101,26 +102,10 @@ export function App() {
             Reset
           </Button>
         </ButtonGroup>
-      </UiForm>
+      </CommonUiForm>
     </div>
   );
 }
-
-const UiForm = React.forwardRef((props: any, ref: any) => {
-  return (
-    <Box
-      as="form"
-      ref={ref}
-      onChange={props.onChange}
-      p={4}
-      borderWidth="1px"
-      rounded="lg"
-      shadow="1px 1px 3px rgba(0,0,0,0.3)"
-    >
-      {props.children}
-    </Box>
-  );
-});
 
 const UiDescribedImage = ({ description, imageSrc }) => {
   return (
